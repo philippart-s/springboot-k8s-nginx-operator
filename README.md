52c1597d94c465dfc4ab76d44dbc0ea9efa21a57
stephane <stephane.philippart@harmonie-mutuelle.fr>
2021-12-01 21:17:45 +0100

    :tada: init

--- README.md
+++
@@ -1,2 +0,0 @@
-# springboot-k8s-nginx-operator
-Simple Sprinboot example to devlop a k8s operator
---
+++ src/test/java/fr/wilda/nginxoperator/NginxOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wilda.nginxoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
