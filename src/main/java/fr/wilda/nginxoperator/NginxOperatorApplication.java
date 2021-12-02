85a6af95ae23ebf6ddbafcf15dbb1ee692941f56
philippart-s <stephane.philippart@harmonie-mutuelle.fr>
2021-12-02 16:14:44 +0100

    :wrench: confs diverses

--- src/main/java/fr/wilda/nginxoperator/NginxOperatorApplication.java
+++ src/main/java/fr/wilda/nginxoperator/NginxOperatorApplication.java
@@ -9,5 +9,4 @@ public class NginxOperatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NginxOperatorApplication.class, args);
 	}
-
 }
--- src/test/java/fr/wilda/nginxoperator/NginxOperatorApplicationTests.java
+++ src/test/java/fr/wilda/nginxoperator/NginxOperatorApplicationTests.java
@@ -3,7 +3,10 @@ package fr.wilda.nginxoperator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.javaoperatorsdk.operator.springboot.starter.test.EnableMockOperator;
+
 @SpringBootTest
+@EnableMockOperator(crdPaths = "classpath:nginxinstallerresources.fr.wilda-v1.yml")
 class NginxOperatorApplicationTests {
 
 	@Test
