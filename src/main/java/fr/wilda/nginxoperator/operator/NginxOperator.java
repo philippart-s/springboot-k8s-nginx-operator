7f8674b8a666aa7cb12d01e09eb3097b972071a3
Stéphane Philippart <stephane.philippart@harmonie-mutuelle.fr>
2021-12-02 12:42:45 +0000

    :wrench:

--- src/main/java/fr/wilda/nginxoperator/operator/NginxOperator.java
+++ src/main/java/fr/wilda/nginxoperator/operator/NginxOperator.java
@@ -1,11 +1,9 @@
 package fr.wilda.nginxoperator.operator;
 
-import org.springframework.stereotype.Component;
 import fr.wilda.nginxoperator.controller.NginxInstallerController;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
 
-@Component
 public class NginxOperator {
 
     private final Operator operator;
